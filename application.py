2c2f91d87f677461a3f35032aceb4438ad29b82d
Nikita <uchihanaruto9371@gmail.com>
2020-03-09 15:09:47 +0200

    setup the application

---
+++ application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.secret_key = 'replace later'
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
