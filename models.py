ebb1e47e531f88b1da424db08859d0831afbbf51
Nikita <uchihanaruto9371@gmail.com>
2020-03-09 18:41:09 +0200

    Connect the database

---
+++ models.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+	""" User Model """
+
+	__tablename__ = "users"
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(25), unique=True, nullable=False)
+	password = db.Column(db.String(), nullable=False)
