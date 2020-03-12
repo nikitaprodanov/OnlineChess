a43c84929ef8983519d2b60c5fc75a5ffc7d55ae
Nikita <uchihanaruto9371@gmail.com>
2020-03-12 13:31:16 +0200

    Flask login session manager

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_login import LoginManager, login_user, current_user
 
 from wtform_fields import *
 from models import *
@@ -10,6 +11,16 @@ app.secret_key = 'replace later'
 # Configure database
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://kcszumbnyngcpe:b6bff3fa03331de5fa50e583ab82b05524c0e1b1551fdc98673a56c558fbff29@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d8ru7iuptl4q1i'
 db = SQLAlchemy(app)
+
+# Configure login manager
+login = LoginManager()
+login.init_app(app)
+
+@login.user_loader
+def load_user(id):
+
+	return User.query.get(int(id))
+
 
 # Registration route
 @app.route("/", methods=['GET', 'POST'])
@@ -38,7 +49,12 @@ def login():
 	login_form = LoginForm()
 
 	if login_form.validate_on_submit():
-		return "Logged in successfully"
+		user_object = User.query.filter_by(username=login_form.username.data).first()
+		login_user(user_object)
+		if current_user.is_authenticated:
+			return "Logged in with flask login"
+
+		return "Not logged in"
 
 	return render_template("login.html", form=login_form)
 
