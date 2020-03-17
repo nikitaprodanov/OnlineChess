f7730aff69929a44784d1771b569c0211607f27d
0NZ1T4M <velislav.boyanov@gmail.com>
2020-03-17 14:12:29 +0200

    Added castling, pawn promotions and checking.

--- input.py
+++ input.py
@@ -1,9 +1,3 @@
-def is_position(pos1, pos2):
-	letters = "abcdefgh"
-	nums = [1, 2, 3, 4, 5, 6, 7, 8]
-	if pos1 in letters:
-		if pos2 in nums:
-			return 1
 
 def input_check(input):
 	numeral_positions = [1, 2, 3, 4, 5, 6, 7, 8]
@@ -14,14 +8,14 @@ def input_check(input):
 		#is the figure real
 		if input[1] in figures:
 			#is the position real
-			if is_position(input[2], input[3]) == 1:
-				move = "Simple"
+			if input[2] in letteral_positions and input[3] in numeral_positions:
+				move = "MoveS"
 
 	#is it a pawn move
 	if len(input) == 2:
 		if input[1] == e:
 			if input[2] in numeral_positions:
-				move = "PawnMove"
+				move = "MoveP"
 
 	#is it a complex move(same type of pieces)
 	if len(input) == 4:
@@ -30,12 +24,12 @@ def input_check(input):
 			#is the fig position real_letter
 			if input[2] in letteral_positions:
 				#is the position real
-				if is_position(input[3], input[4]) == 1:
+				if input[3] in letteral_positions and input[4] in numeral_positions:
 					move = "ComplexMoveL"
 			#is the figure real_number
 			elif input[2] in numeral_positions:
 				#is the position real
-				if is_position(input[3], input[4]) == 1:
+				if input[3] in letteral_positions and input[4] in numeral_positions:
 					move = "ComplexMoveN"
 					#If both figs are placed on the same letter-file,
 					#one on d4 and the other one on d2, you write down N4f3 or N2f3,
@@ -47,17 +41,41 @@ def input_check(input):
 		if input[1] in figures:
 			#is it an attack move
 			if input[2] == 'x':
-				if is_position(input[3], input[4]) == 1:
+				if input[3] in letteral_positions and input[4] in numeral_positions:
 					move = "Capture"
 		elif input[1] in letteral_positions:
 			#is it an attack move
 			if input[2] == 'x':
 				if input[3] == 'e':
 					if input[4] in numeral_positions:
-						move = "CapturePawn"
+						move = "CaptureP"
 
+	#is it a castling
+	if input == "0-0":
+		move = "CastlingK"
 
- 
+	if input == "0-0-0":
+		move = "CastlingQ"
+
+	#is it a check
+	if len(input) == 3:
+		if input[1] in figures:
+			if input[2] in letteral_positions and input[3] in numeral_positions:
+				if input[4] == '+':
+					move = "Check"
+	elif len(input) == 4:
+		if input[1] in figures:
+			if input[2] in letteral_positions and input[3] in numeral_positions:
+				if input[4] == '+' and input[5] == '+':
+					move = "CheckD"
+
+	#is it a promotion
+	if len(input) == 3:
+		if input[1] == 'e':
+			if input[2] in numeral_positions:
+				if input[3] in figures and input[3] != 'e':
+					move = "Promotion"
+			
 
 
 
