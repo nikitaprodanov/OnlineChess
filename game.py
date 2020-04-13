b47608540086d71c12478ed71fd0fac8fc22d6a6
0NZ1T4M <velislav.boyanov@gmail.com>
2020-04-13 18:49:57 +0300

    Started game input and board printing after move.

--- game.py
+++ game.py
@@ -1,8 +1,39 @@
 import chess 
 
-board = chess.Board()
-
 def game(input):
+	board = chess.Board()
+	result = ""
+	if input == "/start/":
+		result = start_game(board)
+
+	use_input = input.replace('/', '')
+	if use_input in list_of_moves(board):
+		result = move(use_input, board)
+
+	return result
+
+def list_of_moves(board):
+	moves = []
+	for move in board.legal_moves:
+		moves.append(str(move))
+
+	return moves
+
+def start_game(board):
+	result = ""
+	board = chess.Board()		
+	result = print_board(board)
+	return result
+
+def move(input, board):
+ 	result = ""
+ 	# del input[0]
+ 	# del input[-1]
+ 	board.push_san(str(input))
+ 	result = print_board(board)
+ 	return result
+
+def print_board(board):
 	string_board = str(board)
 	result_board = []
 	line = ""
