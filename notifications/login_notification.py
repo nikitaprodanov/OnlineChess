05ee34450d20f041d643051eecc64b953c343e68
Nikita <uchihanaruto9371@gmail.com>
2020-06-02 12:20:11 +0300

    Login notification

---
+++ notifications/login_notification.py
@@ -0,0 +1,11 @@
+from plyer import notification
+
+def login_correct {
+    notification.notify(
+        title = "OnlineChess",
+        message = "You logged in successfuly",
+        app_name = "OnlineChess",
+        timeout = 10,
+        toast = True
+    )
+}
