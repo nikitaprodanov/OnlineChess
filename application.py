a1a0dee77479c7f62d34f4b2e1a414b1d4da78fc
Nikita <uchihanaruto9371@gmail.com>
2020-06-08 17:12:02 +0300

    Using heroku config vars instead of db link

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import os
+
 from time import localtime, strftime
 
 from flask import Flask, render_template, redirect, url_for
@@ -23,7 +25,7 @@ app = Flask(__name__)
 app.secret_key = 'replace later'
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']='postgres://kcszumbnyngcpe:b6bff3fa03331de5fa50e583ab82b05524c0e1b1551fdc98673a56c558fbff29@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d8ru7iuptl4q1i'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
 # Initialize Flask-SocketIO
