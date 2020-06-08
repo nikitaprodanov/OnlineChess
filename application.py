e7dc9293153c80bdc3c25a20e9b5d605d7fc23c6
Nikita <uchihanaruto9371@gmail.com>
2020-06-08 17:38:58 +0300

    Heroku

--- application.py
+++ application.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET')
 
 # Configure database
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI']='postgres://kcszumbnyngcpe:b6bff3fa03331de5fa50e583ab82b05524c0e1b1551fdc98673a56c558fbff29@ec2-35-168-54-239.compute-1.amazonaws.com:5432/d8ru7iuptl4q1i'
 db = SQLAlchemy(app)
 
 # Initialize Flask-SocketIO
